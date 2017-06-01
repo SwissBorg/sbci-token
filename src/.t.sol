984fb0d28480a50a9d05dced51f146a1bb8215c7
drgorb <micha.roon@managination.com>
2017-06-01 16:52:31 +0200

    dapp init ico-fund

---
+++ src/.t.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.8;
+
+import "ds-test/test.sol";
+
+import "./.sol";
+
+contract Test is DSTest {
+    function test_basic_sanity() {
+        assert(true);
+    }
+
+    function testFail_basic_sanity() {
+        assert(false);
+    }
+}
